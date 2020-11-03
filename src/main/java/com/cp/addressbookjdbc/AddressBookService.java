f3d2afda506b342b2cd7d22444ad839a8e4bdbd6
akinator001 <aakashgandhi1279@gmail.com>
2020-11-04 00:35:22 +0530

    [aakash] Updated Details and Sync with Database

--- src/main/java/com/cp/addressbookjdbc/AddressBookService.java
+++ src/main/java/com/cp/addressbookjdbc/AddressBookService.java
@@ -6,23 +6,37 @@ public class AddressBookService {
 	public enum IOService {
 		CONSOLE_IO, FILE_IO, DB_IO, REST_IO
 	}
-
 	private List<AddressBookData> addBookList;
 	private AddressBookDBService addBookDB;
-
+	
 	public AddressBookService() {
 		addBookDB = AddressBookDBService.getInstance();
 	}
-
+	
 	public AddressBookService(List<AddressBookData> addBookList) {
 		this();
 		this.addBookList = addBookList;
 	}
-
+	
 	public List<AddressBookData> readAddresBookData(IOService ioService) {
 		if(ioService.equals(IOService.DB_IO)) {
 			this.addBookList = addBookDB.readData();
 		}
 		return this.addBookList;
+	}
+
+	public void updateContactsCity(String firstName, String city) {
+		int result = addBookDB.updateData(firstName, city);
+		if(result == 0)	return;
+		AddressBookData addBookData = this.checkAddressBookDataInSyncWithDB(firstName);
+		if(addBookData != null)	addBookData.city = city;
+	}
+
+	public AddressBookData checkAddressBookDataInSyncWithDB(String firstName) {
+		List<AddressBookData> addBookDataList = addBookDB.getAddressBookData(firstName);
+		return addBookDataList.stream()
+				  .filter(con -> con.firstName.equals(firstName))
+				  .findFirst()
+				  .orElse(null);
 	}
 }
